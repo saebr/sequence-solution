957247ee57997a56b2876a09e1dc4b035eb8b64d
Ben Levin <benlevin@Bens-MacBook-Pro.local>
2022-09-21 04:56:02 -0500

    Added event listener when clicking submit button and capturing values of numbers inserted in input boxes

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+function run() {
+    console.log('button clicked')
+    var a = Number(document.getElementById('input1').value)
+    var b = Number(document.getElementById('input2').value)
+    console.log(a + b)
+}
