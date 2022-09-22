3d1b988b63abd83c1a34a4d329e798a1affcf26f
Ben Levin <benlevin@Bens-MacBook-Pro.local>
2022-09-21 22:10:25 -0500

    Added backend logic to determine to skip or add jobs.

--- script.js
+++ script.js
@@ -4,5 +4,15 @@ function run() {
     console.log('button clicked')
     var a = Number(document.getElementById('input1').value)
     var b = Number(document.getElementById('input2').value)
-    console.log(a + b)
+    var dif = a - b
+    var fix
+    console.log(dif)
+    if (a === b) {
+        fix = "In sequence"
+    } else if (a > b) {
+        fix = "Create " + dif + " GTX"
+    } else if (a < b) {
+        fix = "Skip " + dif + " GTX" 
+    }
+console.log(fix)
 }
