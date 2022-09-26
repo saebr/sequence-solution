9297ba566653e563211384d62b4ac1fb9b7e9bbe
Ben Levin <benlevin@Bens-MacBook-Pro.local>
2022-09-26 01:12:13 -0500

    Added error handling to check if proper dropdowns are selected

--- script.js
+++ script.js
@@ -2,17 +2,35 @@
 
 function run() {
     console.log('button clicked')
-    var a = Number(document.getElementById('input1').value)
-    var b = Number(document.getElementById('input2').value)
-    var dif = a - b
+    var input1 = Number(document.getElementById('input1').value)
+    var input2 = Number(document.getElementById('input2').value)
+    var dropdown1 = document.getElementById('dropdown1')
+    var dropdown2 = document.getElementById('dropdown2')
+    var dif = input1 - input2
     var fix
     console.log(dif)
-    if (a === b) {
-        fix = "In sequence"
-    } else if (a > b) {
-        fix = "Create " + dif + " GTX"
-    } else if (a < b) {
-        fix = "Skip " + dif + " GTX" 
-    }
-console.log(fix)
+    console.log(dropdown1.value)
+    console.log(dropdown2.value)
+    
+    //Error handeling if Chassis is selecte in both boxes
+    if (dropdown1.value == 'CP' & dropdown2.value == 'CP') {
+        console.log('Cannot compare Chassis to Chasis')
+
+    //Error handling if SP, FS, or RS are selected to compare to Body dropdown.
+    } else if (dropdown1.value == 'FS' & dropdown2.value == 'VIN'){
+        console.log('Cannot compare Front Subrame to Body')
+    } else if (dropdown1.value == 'RS' & dropdown2.value == 'VIN'){
+        console.log('Cannot compare Rear Subrame to Body')
+    } else if (dropdown1.value == 'SP' & dropdown2.value == 'VIN'){
+        console.log('Cannot compare Battery Pack to Body')
+    } else
+    
+        if (input1 === input2) {
+            fix = "In sequence"
+        } else if (input1 > input2) {
+            fix = "Create " + dif + " GTX"
+        } else if (input1 < input2) {
+            fix = "Skip " + dif + " GTX"
+        }
+    console.log(fix)
 }
